bcf696d04cfca2246a2f0c7f0b1b645af9cd5a34
ABoni14 <aboni150@gmail.com>
2021-11-12 15:35:55 +0100

    add insert item with "enter" and button, last commit

---
+++ MILESTONE 3/script.js
@@ -0,0 +1,60 @@
+const app = new Vue({
+  el:'#app',
+
+  data: {
+
+    todoList:
+    [
+      {
+        frase: 'Andare a fare la spesa',
+        isUnderscore: false
+      },
+      {
+        frase: 'Studiare per il corso Boolean',
+        isUnderscore: false
+      },
+      {
+        frase: 'Pagare affitto casa',
+        isUnderscore: false
+      },
+      {
+        frase: 'Andare a fare la spesa',
+        isUnderscore: false
+      },
+    ],
+
+    stringToDo: 
+    {
+      frase: "",
+      isUnderscore: false,
+    },
+    errorMsg: false,
+  },
+
+  methods: {
+    removeItem(index){
+      this.todoList.splice(index, 1);
+    },
+
+    insertItem(){
+      if(this.stringToDo.length < 2) {
+        this.errorMsg = true;
+        setTimeout(() =>{
+          this.errorMsg = false;
+        },5000);
+      }else{
+        this.todoList.push(this.stringToDo);
+        this.stringToDo = 
+        {
+          frase: "",
+          isUnderscore: false,
+        };
+        this.errorMsg = false;
+        console.log(this.todoList);
+        console.log(this.toDoList);
+      }
+    },
+
+    }
+  }
+)
